971975fac18ff6a9d768f64540faee641ec2dc7d
Larissa Schwab <larissa.schwab@yahoo.com.br>
2025-06-04 20:04:52 -0300

    Adicionado

---
+++ src/escola_api/database/banco_dados.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+# carregar o arquivo .env com as configurações do banco de dados
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = os.getenv("DB_PORT", 3306)
+DB_NAME = os.getenv("DB_NAME", "escola-db")
+
